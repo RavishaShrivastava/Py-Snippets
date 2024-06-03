694ca0c2fd2ff64bce76259ccc139f7c41245a47
Ravisha Shrivastava <151850353+RavishaShrivastava@users.noreply.github.com>
2024-06-04 02:04:17 +0530

    Update paras.py

--- Typing Test/paras.py
+++ Typing Test/paras.py
@@ -1,14 +1,17 @@
-import random
+#importing modules
+import random 
 import fontstyle
-import time
-import timer
+import time #user-defined 
+import timer #user-defined
+
 def paragraphs():
     p='''The advent of Artificial Intelligence (AI) has revolutionized the way we live, work, and communicate.One of the most significant applications of AI is in the realm of writing, particularly in creating essays.AI-powered tools can generate essays quickly and efficiently, but they often lack the human touch, making them less relatable and less engaging.In this essay, we will explore the concept of AI and human easy essay, highlighting the benefits and limitations of using AI in essay writing.The future of AI and human easy essay lies in a harmonious blend of both.AI-powered tools can assist writers in generating ideas, outlining, and structuring essays, while human writers can add the creative touch, nuance, and emotional depth that AI lacks.This collaboration can lead to high-quality, engaging, and well-written essays that showcase the best of both worlds.While AI-powered tools can generate essays quickly and efficiently, they often lack the creativity, nuance, and emotional depth that human writers bring to the table.AI-generated essays may sound robotic and lack the personal touch, making them less engaging and less relatable.Moreover, AI tools may not be able to understand the context and subtleties of human language, leading to inaccuracies and misunderstandings.'''
-    
+
+    #sentences to display for typing
 
     l=p.split('.')
-    
-    n=random.choice(l)
+    #splitting by '.' 
+    n=random.choice(l) #anyone selected at random
 
     return n
 
